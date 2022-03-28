4ffe8449b578e792e3769e3aef5f4357707195e9
Micael Quilião Gerson <micaelquiliao20@gmail.com>
2022-03-28 15:18:17 -0300

    Adicionar arquivos de projeto.

---
+++ lambdaTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace lambdaTest
+{
+    class Program
+    {
+        static Func<string, int> tamanhoTextoLambda = (text => text.Length);
+
+        static void Main(string[] args)
+        {
+            int tamanhoNome = tamanhoTextoLambda("Micael Quilião Gerson");
+            Console.WriteLine("Micael Quilião Gerson tem " + tamanhoNome + " caracteres.");
+
+            Console.ReadLine();
+        }
+    }
+}
