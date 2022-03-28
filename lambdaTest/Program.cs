9c5be59fb57fa54a49302777edbb9865b5285cd9
Micael Quilião Gerson <micaelquiliao20@gmail.com>
2022-03-28 15:57:22 -0300

    Adicionada mais uma Func de exemplo

--- lambdaTest/Program.cs
+++ lambdaTest/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
 using static System.Console;
 
 namespace lambdaTest
@@ -7,10 +11,17 @@ namespace lambdaTest
     {
         static Func<string, int> tamanhoTextoLambda = (text => text.Length);
 
+        static Func<int, int> quadrado = (num => num * num);
+
         static void Main(string[] args)
         {
             int tamanhoNome = tamanhoTextoLambda("Micael Quilião Gerson");
             Console.WriteLine("Micael Quilião Gerson tem " + tamanhoNome + " caracteres.");
+
+            IEnumerable<int> numeros = new List<int> { 2, 3, 4, 5 };
+
+            var quadradoNumeros = numeros.Select(quadrado);
+            Console.WriteLine(string.Join(", ", quadradoNumeros));
 
             Console.ReadLine();
         }
